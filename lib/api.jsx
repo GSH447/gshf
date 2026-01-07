72af21ea1220dd91a5f4f84ac940b4d0945fa085
Orodje <akpotororodje@gmail.com>
2026-01-07 14:28:08 +0100

    dev mode

--- lib/api.jsx
+++ lib/api.jsx
@@ -1,12 +1,12 @@
 // export const API_BASE_URL = 'https://gracespringhospitals.com/api';
 
-export const API_BASE_URL = 'http://foundation.gracespringhospitals.com:8081/api/';
+// export const API_BASE_URL = 'http://foundation.gracespringhospitals.com:8081/api/';
 
 // export const API_BASE_URL = 'https://foundation.gracespringhospitals.com:8081/api/';
 
 // export const API_BASE_URL = 'http://foundation.gracespringhospitals.com/api/';
 
-// export const API_BASE_URL = 'https://foundation.gracespringhospitals.com/api/';
+export const API_BASE_URL = 'https://foundation.gracespringhospitals.com/api/';
 
 // export const API_BASE_URL = 'http://localhost/gshf/gshf-mvc/public';
 
